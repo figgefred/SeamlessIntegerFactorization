a9ffeb4099cdf5f1b35fc7906480ffd241dc2921
Joakim Uddholm <joakim@uddholm.com>
2013-11-04 17:41:08 +0100

    wooo verkar fungera

--- src/factoring.go
+++ src/factoring.go
@@ -19,9 +19,9 @@ func pollardRho(toFactor *big.Int, f polynomial) (*big.Int, bool) {
 	for(d.Cmp(big.NewInt(1)) == 0) {
 		x = f(x) 
 		y = f(f(y))
-		fmt.Println(x)
-		fmt.Println(y)
-		fmt.Println()
+		//~ fmt.Println(x)
+		//~ fmt.Println(y)
+		//~ fmt.Println()
 		result := new(big.Int)
 		result.Sub(x,y)
 		result.Abs(result)
@@ -37,7 +37,7 @@ func pollardRho(toFactor *big.Int, f polynomial) (*big.Int, bool) {
 func get_f(toFactor *big.Int) polynomial {
 	return func(x *big.Int) *big.Int {
 		result := new(big.Int).Mul(x,x)
-		result.Sub(result, big.NewInt(1))
+		result.Add(result, big.NewInt(rng.Int63()))
 		result.Mod(result, toFactor)
 		return result
 	}
@@ -49,11 +49,34 @@ func factorise(toFactor *big.Int) {
 		return
 	}
 	
-	f := get_f(toFactor) //big.NewInt(rng.Int63())
-	factor,result := pollardRho(toFactor, f)
+	quo := new(big.Int)
+	quo.Set(toFactor)
 	
-	fmt.Println(factor)
-	fmt.Println(result)
+	//f := get_f(new(big.Int).Mul(toFactor,toFactor))
+	for(quo.Cmp(big.NewInt(1)) > 0) {
+		f := get_f(toFactor)
+		factor,error := pollardRho(quo, f)
+		
+		if(error) {
+			// Try again
+			continue
+		}
+		
+		quo.Quo(quo, factor)				
+		
+		if(!factor.ProbablyPrime(20)) {
+			factorise(factor)
+		} else {
+			fmt.Println(factor) //.String() + ", " + quo.String())
+		}
+		
+		if(quo.ProbablyPrime(20)) {
+			fmt.Println(quo)
+			return
+		}
+		
+		
+	}
 }
 
 func main() {
