ea7213352192d6946666e89ad6573b4197dcdf64
Joakim Uddholm <joakim@uddholm.com>
2013-11-04 17:18:13 +0100

    fungerar med bigint

--- src/factoring.go
+++ src/factoring.go
@@ -2,55 +2,63 @@ package main
 
 import "fmt"
 import "math/big"
+import "math/rand"
+import "time"
 
-func gcd(a big.Int, b big.Int) float64 {
-	if(b == 0) {
-		return a
-	}
-	if(a > b) {
-		return gcd(a-b, b)
-	} else if(a < b) {
-		return gcd(a,b-a)
-	} else {
-		return a
-	}
-	
-	return 1
-}
+type polynomial func(*big.Int) *big.Int
 
-type polynomial func(big.Int) big.Int
+var (
+	rng = rand.New(rand.NewSource(time.Now().UnixNano()))
+)
 
-func pollardRho(toFactor big.Int, f polynomial) (big.Int, bool) {
-	var x,y,d big.Int
-	x = 2
-	y = 2
-	d = 1
-	for(d == 1) {
+func pollardRho(toFactor *big.Int, f polynomial) (*big.Int, bool) {
+	var x,y,d *big.Int
+	x = big.NewInt(2)
+	y = big.NewInt(2)
+	d = big.NewInt(1)
+	for(d.Cmp(big.NewInt(1)) == 0) {
 		x = f(x) 
 		y = f(f(y))
-		d = gcd(big.Abs(x-y),toFactor)
+		fmt.Println(x)
+		fmt.Println(y)
+		fmt.Println()
+		result := new(big.Int)
+		result.Sub(x,y)
+		result.Abs(result)
+		d = result.GCD(nil, nil, result, toFactor)
 	}
-	if(d == toFactor) {
+	if(d.Cmp(toFactor) == 0) {
 		return d, true
 	}
 	
 	return d, false
 }
 
-func get_f(toFactor big.Int) polynomial {
-	return func(x big.Int) big.Int {
-		return big.Mod(x*x - 1, toFactor)
+func get_f(toFactor *big.Int) polynomial {
+	return func(x *big.Int) *big.Int {
+		result := new(big.Int).Mul(x,x)
+		result.Sub(result, big.NewInt(1))
+		result.Mod(result, toFactor)
+		return result
 	}
 }
 
-func factorise(toFactor float64) {
-	// factors := make([]int, 20)
-		
+func factorise(toFactor *big.Int) {	
+	if(toFactor.ProbablyPrime(20)) {
+		fmt.Println(toFactor)
+		return
+	}
+	
+	f := get_f(toFactor) //big.NewInt(rng.Int63())
+	factor,result := pollardRho(toFactor, f)
+	
+	fmt.Println(factor)
+	fmt.Println(result)
 }
 
 func main() {
-	numberToFactor = new(big.Int)
-	fmt.Scan(&toFactor)
-
-	fmt.Println(pollardRho(toFactor,  get_f(toFactor)))
+	toFactor := new(big.Int)
+	//fmt.Println(rng.Int31())
+	fmt.Scan(toFactor)		
+	factorise(toFactor)
 }
