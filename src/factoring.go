a366156edaa877477494e5ae3ca870b1c138d74a
Frederick Ceder <frederickceder@gmail.com>
2013-11-05 23:37:26 +0100

    1 at kattis :D ... Körs parallelt - Naiv implementation på timeouts

--- src/factoring.go
+++ src/factoring.go
@@ -8,30 +8,165 @@ import "runtime"
 import "bufio"
 import "os"
 import "strings"
+//import "strconv"
+
+type task struct {
+	index int
+	toFactor big.Int
+}
+
+type partResult struct {
+	index int
+	factor *big.Int
+}
 
 type polynomial func(*big.Int) *big.Int
 
+
 var (
+	totalRunTime int = 15000 // milliseconds
+
+	numWorkers int
+	inputsize int
 	rng = rand.New(rand.NewSource(time.Now().UnixNano()))
-	deadline = 5
+	deadline = 10
 	prime_precision = 20
+	capacity = 100
+	
+	resultChannel = make(chan [][]*partResult)
+
+	resultSubmission chan []*partResult
+	taskChannel chan *task
 )
 
-func pollardRho(toFactor *big.Int, f polynomial) (*big.Int, bool) {
+// A function that should be runned in a go routine.
+// This function initializes new worker Go-routines 
+// for every task.
+// It will only do NumProcs workers at a time.
+func coordinate(tasks []*task, finishedChan *chan bool) {
+
+	// Reinitialize submission channel
+	resultSubmission = make(chan []*partResult, numWorkers)
+
+	// Init collection that will hold results
+	results := make([][]*partResult, inputsize)
+	for i:= 0; i < inputsize; i++ {
+		results[i] = make([]*partResult, 0, capacity)
+	}
+	
+	// Some counters
+	taskChannelSpace := numWorkers
+	nextTask := 0
+	resultsReceived := 0
+	runTime := totalRunTime
+//	initTime := time.Now()
+
+	// Receive and save results and create new tasks if possible until done
+	done := false
+	for !done {
+		select {
+			case result, open := <- resultSubmission:
+				if open {
+					for _, res := range result {
+						if res.factor == nil {
+							results[res.index] = nil	
+							////fmt.Println("Coordinator:", "Failed task", res.index)			
+							break
+						}
+						////fmt.Println("Coordinator:", "Received result", res.index)			
+						results[res.index] = append(results[res.index], res)
+					}
+					resultsReceived++
+				}
+			default:
+				channelSpace := taskChannelSpace - len(taskChannel)
+				if nextTask < len(tasks) && channelSpace > 0 {
+					for i := 0; i < channelSpace; i++ {
+						if nextTask == len(tasks) {
+							break
+						}
+						t := tasks[nextTask]
+						nextTask += 1
+						runTime = totalRunTime/(len(tasks)-resultsReceived)
+						timeout := time.Duration(runTime)*time.Millisecond
+						////fmt.Println("Coordinator:", "Setting timelimit @", timeout, "for Worker", nextTask)
+						start := time.Now()
+						go work(start, timeout, *t)
+					}
+				} else if nextTask == len(tasks) && resultsReceived == len(tasks) {
+					////fmt.Println("Coordinator:", "Now we are done")
+					done = true
+				} else {
+					////fmt.Println("Coordinator:", "Nothing to do")
+					runtime.Gosched()
+				}
+		}
+	}
+	close(resultSubmission)
+	//elapsedTime := time.Since(initTime)
+	// Dump out to sys out
+	printResult(results)
+	////fmt.Println("Coordinator:", "Finished after", elapsedTime)
+	*finishedChan <- true
+}
+
+func work(start time.Time, timeout time.Duration, task task) {
+	
+	// Do task if it is not nil
+	rawResult := factorise(start, timeout, &task.toFactor)
+	result := make([]*partResult, 0, 100)
+	if rawResult == nil {
+		res := partResult{task.index, nil}
+		result = append(result, &res)
+	//	//fmt.Println("Worker:", "Exeeded time limit of", timeout)
+	}
+	for _, rawRes := range rawResult {
+		res := partResult{task.index, rawRes}
+		result = append(result, &res)
+	}
+	// Send to coordinator
+	resultSubmission <- result
+
+}
+
+func printResult(resultCollection [][]*partResult) {
+	for _, results := range resultCollection {
+		if results == nil {
+			fmt.Println("fail")
+			fmt.Println("")
+			continue
+		}
+		for _, res := range results {
+			if res.factor.Cmp(big.NewInt(0)) == 0 {
+				fmt.Println("fail")
+				break
+			}
+			fmt.Println(res.factor)
+		}
+		fmt.Println("")
+	}
+}
+
+func pollardRho(start time.Time, timeout time.Duration, toFactor *big.Int, f polynomial) (*big.Int, bool) {
 	var x,y,d *big.Int
 	x = big.NewInt(2)
 	y = big.NewInt(2)
 	d = big.NewInt(1)
-	for(d.Cmp(big.NewInt(1)) == 0) {
+	currTime := time.Now()
+	for(currTime.Sub(start) < timeout && d.Cmp(big.NewInt(1)) == 0) {
 		x = f(x) 
 		y = f(f(y))
-		//~ fmt.Println(x)
-		//~ fmt.Println(y)
-		//~ fmt.Println()
-		result := new(big.Int)
-		result.Sub(x,y)
-		result.Abs(result)
-		d = result.GCD(nil, nil, result, toFactor)
+		//~ ////fmt.Println(x)
+		//~ ////fmt.Println(y)
+		//~ ////fmt.Println()
+		r := new(big.Int)
+		r.Sub(x,y)
+		r.Abs(r)
+		d = r.GCD(nil, nil, r, toFactor)
+		currTime = time.Now()
+	}
+	if currTime.Sub(start) > timeout {
+		return nil, true
 	}
 	if(d.Cmp(toFactor) == 0) {
 		return d, true
@@ -42,93 +177,125 @@ func pollardRho(toFactor *big.Int, f polynomial) (*big.Int, bool) {
 
 func get_f(toFactor *big.Int) polynomial {
 	return func(x *big.Int) *big.Int {
-		result := new(big.Int).Mul(x,x)
-		result.Add(result, big.NewInt(rng.Int63()))
-		result.Mod(result, toFactor)
-		return result
+		r := new(big.Int).Mul(x,x)
+		r.Add(r, big.NewInt(rng.Int63()))
+		r.Mod(r, toFactor)
+		return r
 	}
 }
 
-func factorise(toFactor *big.Int) string {	
+func factorise(start time.Time, timeout time.Duration, toFactor *big.Int) []*big.Int {	
+	buffer := make([]*big.Int, 0, 100)
 	if(toFactor.ProbablyPrime(prime_precision)) {
-		return toFactor.String() + "\n"
+		return append(buffer, toFactor)
 	}
 	
 	quo := new(big.Int)
 	quo.Set(toFactor)
-	
-	var buf string
-	//f := get_f(new(big.Int).Mul(toFactor,toFactor))
-	for(quo.Cmp(big.NewInt(1)) > 0) {
+	currTime := time.Now()
+	for(currTime.Sub(start) < timeout && quo.Cmp(big.NewInt(1)) > 0) {
+
 		f := get_f(toFactor)
-		factor,error := pollardRho(quo, f)
+		factor, error := pollardRho(start, timeout, quo, f)
 		
-		if(error) {
+		if(error || factor.Int64() == int64(0)) {
 			// Try again
+			currTime = time.Now()
 			continue
 		}
 		
-		quo.Quo(quo, factor)				
-		
-		if(!factor.ProbablyPrime(prime_precision)) {
-			buf += factorise(factor)
-		} else {
-			buf += factor.String() + "\n" 
-		}
-		
-		if(quo.ProbablyPrime(prime_precision)) {
-			buf += quo.String() + "\n"
-			break
-		}
-	}
-	
-	return buf
-}
+        quo.Quo(quo, factor)                                
+        
+        if(!factor.ProbablyPrime(prime_precision)) {	
+        	res := factorise(start, timeout, factor)
+        	if res == nil {
+        		return nil
+        	}
+        	for _, r := range res {
+        		buffer = append(buffer, r)
+        	}
+        } else {
+        	buffer = append(buffer, factor)
+        }
 
-var result chan string
+        if(quo.ProbablyPrime(prime_precision)) {
+            buffer = append(buffer, quo)
+            break
+        }
+        currTime = time.Now()
+	}
+	if currTime.Sub(start) > timeout {
+		return nil
+	}
+	// Lets redo this - send back old task for hope of better function
+	return buffer
+}
 
 func main() {
 		
 	reader := bufio.NewReader(os.Stdin)
 
-	//factorCount := 100
-	factorCount := 2
-	factorValues := make([]*big.Int, factorCount)
+	factorCount := 100
+	factorValues := make([]*task, 0, factorCount)
 	// Read in line by line
-    for i := 0; i < factorCount; i++ {
+	for i:=0; ; i++ {
         line, _ := reader.ReadString('\n')
-        factorValues[i] = new(big.Int)
-        if _, ok := factorValues[i].SetString(strings.TrimSpace(line), 10) ; !ok {
-        	fmt.Println("Parse error of", line)
-        	
+    	if(strings.TrimSpace(line) == "") {
+    		break
+    	}
+
+        factorValue, ok := (new(big.Int)).SetString(strings.TrimSpace(line), 10) 
+        if !ok {
+        	////fmt.Println("Parse error of", line)
 			// Exit
         	return
+        } else {
+        	newTask := new(task)
+        	newTask.index = int(i)
+        	newTask.toFactor = *factorValue
+        	factorValues = append(factorValues, newTask)
         }
     }
+    inputsize = len(factorValues)
 
-	runtime.GOMAXPROCS(runtime.NumCPU())
+    //numProcs := runtime.NumCPU()
+    numProcs := 1
+	runtime.GOMAXPROCS(numProcs)
+
+	numWorkers = numProcs
 	
+	quit := make(chan bool)
+	go coordinate(factorValues, &quit)
+	<- quit
+	/*if( numProcs > 1) {
+		for _, _ = range factorValues {
+			////fmt.Println("fail")
+			////fmt.Println("")
+		}
+		return
+	}*/
+	/*
 	for _, toFactor := range factorValues {
 
 		//timeout := time.After(time.Duration(deadline) * time.Second)
-		result = make(chan string)
+		resultChannel = make(chan string)
 		timeout := make(chan bool, 1)
 		
 		go func() {
-			result <- factorise(toFactor)
+			resultChannel <- factorise(toFactor)
 		}();
 		
 		go func() {
-			time.Sleep(time.Duration(deadline) * time.Second)	
+			time.Sleep(time.Duration(deadline) * time.Millisecond)	
 			timeout <- true
 		}();
 		
 		select {		
-			case factors := <- result:
-				fmt.Println(factors)	
+			case factors := <- resultChannel:
+				////fmt.Println(factors)	
 			case <- timeout:
-				fmt.Println("fail")	
-				fmt.Println()		
+				////fmt.Println("fail")	
+				////fmt.Println()		
 		}
-	}	
+	}*/
 }
